3b96e7cb8eb212c657a28f2ca9275ddd16a58f72
ulysses-ck <ulisesapaza.280@gmail.com>
2022-11-29 19:53:22 -0300

    Finish: base project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'dices';
+	diceLeft = '../assets/img/dice1.png';
+	diceRight = '../assets/img/dice2.png';
+	numOne: number = 1;
+	numTwo: number = 2;
+
+	rollDices(): void {
+		this.numOne = Math.round(Math.random() * 5) + 1;
+		this.numTwo = Math.round(Math.random() * 5) + 1;
+
+		this.diceLeft = `../assets/img/dice${this.numOne}.png`;
+		this.diceRight = `../assets/img/dice${this.numTwo}.png`;
+	}
 }
